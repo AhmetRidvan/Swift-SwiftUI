835fd688af932d6be05df082d4034729ee5565c4
Ahmet Rıdvan Ordulu <ahmetridvanordulu@icloud.com>
2025-08-16 17:19:32 +0300

    sd
    sd

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -8,27 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     @IBOutlet weak var imageViewOne: UIImageView!
 
     @IBOutlet weak var imageViewTwo: UIImageView!
+    
+    var number = 1
+    var number2 = 5
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
        
     }
    
 
     
     @IBAction func hi(_ sender: Any) {
+       
         imageViewOne.image = [
             #imageLiteral(resourceName: "DiceOne"),
             #imageLiteral(resourceName: "DiceTwo"),
             #imageLiteral(resourceName: "DiceThree"),
             #imageLiteral(resourceName: "DiceFour"),
             #imageLiteral(resourceName: "DiceFive"),
-            #imageLiteral(resourceName: "DiceSix") ,
-        ].randomElement()
+            #imageLiteral(resourceName: "DiceSix"),
+        ][number]
+
+        imageViewTwo.image = [
+            #imageLiteral(resourceName: "DiceOne"),
+            #imageLiteral(resourceName: "DiceTwo"),
+            #imageLiteral(resourceName: "DiceThree"),
+            #imageLiteral(resourceName: "DiceFour"),
+            #imageLiteral(resourceName: "DiceFive"),
+            #imageLiteral(resourceName: "DiceSix"),
+        ][number2]
+        
+        
+        
+        if number < 5{
+           number += 1
+        }
+        if number2 > 0{
+            number2 -= 1
+        }
+        
     }
     
 }
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,2 @@
 import UIKit
 
-print("The result of 10+6 = \(10+6)")
-
